0502900d05fb9c7d100b77ecdd61ed34f71fcfd6
shokhDeveloper <shohijahonmusinkulov@gmail.com>
2024-11-14 16:10:19 +0300

    Add all price

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ function App() {
   const telegram = window.Telegram.WebApp;
   const [data, setData] = useState([]);
   let [shopItems, setShopItems] = useState([]);
+  const [allPrice, setAllPrice] = useState(0);
+  const handleCheckPrices = () => {
+    let allPrice = 0;
+    if(shopItems.length){
+      allPrice = shopItems.reduce((acc, item) => {
+        acc+= item.price * item.quantity 
+        return acc
+      }, 0) ;
+    }else return setAllPrice(0)
+    setAllPrice(allPrice)
+  }
   const addItem = (id) => {
     let findShopItem = shopItems.find((shop) => shop.id == id);
     if(findShopItem){ 
         const idx = shopItems.findIndex((shop) => shop.id == id)
         shopItems[idx].quantity += 1;
+
         setShopItems(shopItems)
       }else{  
       findShopItem = data.find((shop) => shop.id == id)  
       shopItems = [...shopItems, {...findShopItem, quantity: 1}]  
       setShopItems(shopItems)
     } 
+    handleCheckPrices()
   };
   const removeItem = (id) => {
     const findShopItemIdx = shopItems.findIndex((shop) => shop.id == id);
@@ -26,11 +39,13 @@ function App() {
       }else shopItems.splice(findShopItemIdx, 1);
     }
     setShopItems(shopItems);
+    handleCheckPrices()
   }
   const handleTelegramApp = () => {
     telegram.MainButton.text = "Sotib olish :)";
     telegram.MainButton.show();
   }
+
   useEffect(() => {
     setData(getData());
   }, []);
@@ -39,7 +54,7 @@ function App() {
       <div className="parent_info">
         <h1>Online Fast Food</h1>
         <div className="parent_price_info">
-          <p>Umumiy narx: <span>12 so'm</span></p>
+          <p>Umumiy narx: <span>{allPrice} so'm</span></p>
           <button onClick={handleTelegramApp}>Buyurtma</button>
         </div>
       </div>
